6fbad3c53bb9c005a3ab3b05031b3fe8208e0535
Safia Chettih <safiamcphd@gmail.com>
2021-12-20 22:58:47 +0000

    Lesson 4 First Exercise
    First & second exercise in using scriptmode

---
+++ script_mode.py
@@ -0,0 +1,18 @@
+from sagemaker.pytorch import PyTorch
+from sagemaker import get_execution_role
+
+hyperparameters = {"epochs": "2", "batch-size": "32", "test-batch-size": "100", "lr": "0.001"} # TODO: Include the hyperparameters your script will need over here.
+
+estimator = estimator = PyTorch(
+    entry_point="scripts/pytorch_mnist.py",
+    base_job_name="sagemaker-script-mode",
+    role=get_execution_role(),
+    instance_count=1,
+    instance_type="ml.m5.large",
+    hyperparameters=hyperparameters,
+    framework_version="1.8",
+    py_version="py36",
+)# TODO: Create your estimator here. You can use Pytorch or any other framework.
+
+#TODO: Start Training
+estimator.fit(wait=True)
